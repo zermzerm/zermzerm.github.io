ecff2a9500c1d5bfeaa10576469e96d2e423b1b6
zermzerm <zerm0907@gmail.com>
2023-08-08 15:52:02 +0900

    scs

--- javaScript/test22.js
+++ javaScript/test22.js
@@ -1,18 +1,19 @@
-function solution(n){
-    var arr=[],cnt=0;
-    for(i=1;i<n+1;i++){
-        var a=0;
-        for(j=i;j<n+1;j++){
-            a+=j;
-            if(a===n){
-                cnt++;
-            }
-            else if(a>n){
-                break;
-            }
-        }
+//
+function solution(n) {
+  var arr = [],
+    cnt = 0;
+  for (i = 1; i < n + 1; i++) {
+    var a = 0;
+    for (j = i; j < n + 1; j++) {
+      a += j;
+      if (a === n) {
+        cnt++;
+      } else if (a > n) {
+        break;
+      }
     }
-    return console.log(cnt);
+  }
+  return console.log(cnt);
 }
 
 solution(14);
